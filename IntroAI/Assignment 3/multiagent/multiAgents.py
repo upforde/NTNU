dc6d8efed35431e7618db301fe903d7cb5534f24
upforde <upforde1@gmail.com>
2020-10-01 14:54:46 +0200

    AlphaBetaPruning completed

--- IntroAI/Assignment 3/multiagent/multiAgents.py
+++ IntroAI/Assignment 3/multiagent/multiAgents.py
@@ -109,32 +109,19 @@ class MinimaxAgent(MultiAgentSearchAgent):
     """
     Your minimax agent (question 2)
     """
-
+    # This section has been changed from the original multiAgents.py
     def getAction(self, gameState):
         """
         Returns the minimax action from the current gameState using self.depth
         and self.evaluationFunction.
         """
-        #Here are some method calls that might be useful when implementing minimax.
 
-        #gameState.getLegalActions(agentIndex):
-        #Returns a list of legal actions for an agent
-        #agentIndex=0 means Pacman, ghosts are >= 1
-
-        #gameState.generateSuccessor(agentIndex, action):
-        #Returns the successor game state after an agent takes an action
-
-        #gameState.getNumAgents():
-        #Returns the total number of agents in the game
-
-        #gameState.isWin():
-        #Returns whether or not the game state is a winning state
-
-        #gameState.isLose():
-        #Returns whether or not the game state is a losing state
-        
-        #*** YOUR CODE HERE ***
         def minimax(state, depth, agent):
+            """
+            Function minimax takes in the current state of the game, the desired max depth
+            and the agent to get the minimax action for as parameters and runs recursively
+            to return the value of the move as well as the minimax action.
+            """
             # Check if max depth or a terminal state has been reached
             if depth == self.depth or state.isWin() or state.isLose():
                 # then the function returns the value of the current state
@@ -146,10 +133,8 @@ class MinimaxAgent(MultiAgentSearchAgent):
             # Checks what kind of agent it is. 
             # If it's pac-man
             if agent == 0:
-                # Initiate the value to be -infinity
-                v = float('-inf')
-                # Initiate the best move to be None
-                bestMove = None
+                # Initiate the value to be -infinity and the best move to be None
+                v, bestMove = float('-inf'), None
                 # For each move that pac-man can take
                 for action in state.getLegalActions(agent):
                     # check the value of the state that this action will take pac-man to
@@ -157,13 +142,85 @@ class MinimaxAgent(MultiAgentSearchAgent):
                     # if the evaluated value of the current action is greater than
                     # the current highest value
                     if v < evaluation:
-                        # then the current highest value gets set to the
-                        # value of the current action
-                        v = evaluation
+                        # then the current highest value gets set to the value of the current action 
                         # and the best move is set to be the current action
-                        bestMove = action
+                        v, bestMove = evaluation, action
                 # The function returns the highest value and the best move to take
                 return v, bestMove
+            # If the agens is a ghost
+            else:
+                # Initiate the value to be infinity and the best move to be None
+                v, bestMove = float('inf'), None
+                # For each move that a ghost can take
+                for action in state.getLegalActions(agent):
+                    # checking wether the next agent is a ghost or not
+                    nextAgent = agent+1 if agent+1 < state.getNumAgents() else 0
+                    # check wether the depth will increment or not
+                    nextDepth = depth+1 if nextAgent == 0 else depth
+                    # check the value of the state that this action will take the ghost to
+                    evaluation = minimax(state.generateSuccessor(agent, action), nextDepth, nextAgent)[0]
+                    # if the evaluated value of the current action is lower than 
+                    # the current lowest value
+                    if v > evaluation:
+                        # then the current lowest value gets set to the value of the current action 
+                        # and the best move is set to be the current action
+                        v, bestMove = evaluation, action
+                # The function returns the lowest value and the best move to take
+                return v, bestMove
+        
+        # The getAction function runs the minimax function and returns the best action to take
+        return minimax(gameState, 0, 0)[1]
+
+
+
+class AlphaBetaAgent(MultiAgentSearchAgent):
+    """
+    Your minimax agent with alpha-beta pruning (question 3)
+    """
+    # This section has been changed from the original multiAgents.py
+    def getAction(self, gameState):
+        """
+        Returns the minimax action using self.depth and self.evaluationFunction
+        """
+
+        def alphaBeta(state, depth, agent, alpha=float('-inf'), beta=float('inf')):
+            """
+            Function alphaBeta takes in the current state of the game, the desired max depth
+            and the agent to get the alphaBeta pruned minimax action for as parameters. It allso takes in optional
+            parameters for alpha and beta, which are used to see if a tree branch should be pruned or not.
+            The function runs recursively to return the value of the move as well as the alphaBeta pruned minimax action.
+            """
+
+            # Check if max depth or a terminal state has been reached
+            if depth == self.depth or state.isWin() or state.isLose():
+                # then the function returns the value of the current state
+                # it also returns None as the best move to take, because it has reached
+                # a terminal point or the farthest depth, meaning there aren't anymore
+                # moves to take
+                return self.evaluationFunction(state), None
+
+            # Checks what kind of agent it is. 
+            # If it's pac-man
+            if agent == 0:
+                # Initiate the value to be -infinity and the best move to be None
+                v, bestMove = float('-inf'), None
+                # For each move that pac-man can take
+                for action in state.getLegalActions(agent):
+                    # check the value of the state that this action will take pac-man to
+                    evaluation = alphaBeta(state.generateSuccessor(agent, action), depth, 1, alpha, beta)[0]
+                    # if the evaluated value of the current action is greater than
+                    # the current highest value
+                    if v < evaluation:
+                        # then the current highest value gets set to the value of the current action 
+                        # and the best move is set to be the current action
+                        v, bestMove = evaluation, action
+                    # setting alpha to be the biggest between alpha and evaluation
+                    alpha = max(evaluation, alpha)
+                    # if alpha is bigger than beta, then break out of the recursion and effectively prune this branch
+                    if alpha > beta: break
+                # The function returns the highest value and the best move to take
+                return v, bestMove
+
             # If the agens is a ghost
             else:
                 # Initiate the value to be infinity
@@ -177,34 +234,22 @@ class MinimaxAgent(MultiAgentSearchAgent):
                     # check wether the depth will increment or not
                     nextDepth = depth+1 if nextAgent == 0 else depth
                     # check the value of the state that this action will take the ghost to
-                    evaluation = minimax(state.generateSuccessor(agent, action), nextDepth, nextAgent)[0]
+                    evaluation = alphaBeta(state.generateSuccessor(agent, action), nextDepth, nextAgent, alpha, beta)[0]
                     # if the evaluated value of the current action is lower than 
                     # the current lowest value
                     if v > evaluation:
-                        # then the current lowest value gets set to the
-                        # value of the current action
-                        v = evaluation
+                        # then the current lowest value gets set to the value of the current action 
                         # and the best move is set to be the current action
-                        bestMove = action
+                        v, bestMove = evaluation, action
+                    # setting beta to be the smallest between beta and evaluation
+                    beta = min(evaluation, beta)
+                    # if alpha is bigger than beta, then break out of the recursion and effectively prune this branch
+                    if alpha > beta: break
                 # The function returns the lowest value and the best move to take
                 return v, bestMove
-        
-        # The getAction function runs the minimax function and returns the best move to take
-        return minimax(gameState, 0, 0)[1]
 
-
-
-class AlphaBetaAgent(MultiAgentSearchAgent):
-    """
-    Your minimax agent with alpha-beta pruning (question 3)
-    """
-
-    def getAction(self, gameState):
-        """
-        Returns the minimax action using self.depth and self.evaluationFunction
-        """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        # The getAction function runs the alphaBeta function and returns the best action to take
+        return alphaBeta(gameState, 0, 0)[1]
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
     """
