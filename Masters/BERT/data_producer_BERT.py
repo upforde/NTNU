1ba2cc877508d25460cc157f922bb601db1471fc
upforde <upforde1@gmail.com>
2022-03-08 15:47:29 +0100

    preparing for GPT-2

--- Masters/BERT/data_producer_BERT.py
+++ Masters/BERT/data_producer_BERT.py
@@ -1,12 +1,15 @@
+import os
+import string
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from random import random
 import torch
-from transformers import BertTokenizer, BertForMaskedLM, AdamW, TrainingArguments, Trainer
+from transformers import BertTokenizer, BertForMaskedLM, AdamW
 from tqdm import tqdm
 
-PATH = "Data/Beer/train.txt"
+
+PATH = "data/train.txt"
 EPOCHS = 3
 
 class MatchesDataset(torch.utils.data.Dataset):
@@ -80,49 +83,58 @@ with open(PATH) as file:
 
 # Set up the BERT model
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-model = BertForMaskedLM.from_pretrained('bert-base-uncased')
 
-# Creating the inputs tensor
-inputs = tokenizer(matches, return_tensors='pt', max_length=512, truncation=True, padding='max_length')
+if len(os.listdir('./model/') ) != 0: model = BertForMaskedLM.from_pretrained("./model/")
+else:
+    model = BertForMaskedLM.from_pretrained('bert-base-uncased')
 
-# Creating the masked inputs
-inputs['labels'] = inputs.input_ids.detach().clone()
-rand = torch.rand(inputs.input_ids.shape)
-mask_arr = (rand < 0.15) * (inputs.input_ids != 101) * (inputs.input_ids != 0) * (inputs.input_ids != 102)
-selection = [torch.flatten(mask_arr[i].nonzero()).tolist() for i in range(mask_arr.shape[0])]
+    # Creating the inputs tensor
+    inputs = tokenizer(matches, return_tensors='pt', max_length=512, truncation=True, padding='max_length')
 
-# Masking selected items
-for i in range(mask_arr.shape[0]): inputs.input_ids[i, selection[i]] = 103
+    # Creating the masked inputs
+    inputs['labels'] = inputs.input_ids.detach().clone()
+    rand = torch.rand(inputs.input_ids.shape)
+    mask_arr = (rand < 0.15) * (inputs.input_ids != 101) * (inputs.input_ids != 0) * (inputs.input_ids != 102)
+    selection = [torch.flatten(mask_arr[i].nonzero()).tolist() for i in range(mask_arr.shape[0])]
 
-# Creating a dataset for the model to use
-dataset = MatchesDataset(inputs)
-dataloader = torch.utils.data.DataLoader(dataset, shuffle=True)
+    # Masking selected items
+    for i in range(mask_arr.shape[0]): inputs.input_ids[i, selection[i]] = 103
 
-# Not enough GPU power :smiling_face_with_tear:
-# device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-device = torch.device('cpu')
-model.to(device)
+    # Creating a dataset for the model to use
+    dataset = MatchesDataset(inputs)
+    dataloader = torch.utils.data.DataLoader(dataset, shuffle=True)
 
-# Setting the model into training mode
-model.train()
+    # Not enough GPU power :smiling_face_with_tear:
+    #device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    device = torch.device('cpu')
+    model.to(device)
 
-optim = AdamW(model.parameters(), lr=1e-5)
+    # Setting the model into training mode
+    model.train()
 
-# Training the model
-for epoch in range(EPOCHS):
-    loop = tqdm(dataloader, leave=True)
-    for batch in loop:
-        optim.zero_grad()
-        input_ids = batch['input_ids'].to(device)
-        attention_mask = batch['attention_mask'].to(device)
-        labels = batch['labels'].to(device)
+    optim = AdamW(model.parameters(), lr=1e-5)
 
-        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
-        loss = outputs.loss
-        loss.backward()
-        optim.step()
+    # Training the model
+    for epoch in range(EPOCHS):
+        loop = tqdm(dataloader, leave=True)
+        for batch in loop:
+            optim.zero_grad()
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            labels = batch['labels'].to(device)
 
-        loop.set_description(f'Epoch {epoch}')
-        loop.set_postfix(loss=loss.item())
+            outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+            loss = outputs.loss
+            loss.backward()
+            optim.step()
+
+            loop.set_description(f'Epoch {epoch}')
+            loop.set_postfix(loss=loss.item())
+
+    model.save_pretrained("./model/")
 
 #TODO: Figure out how to get the model to predict stuff
+inputstring = "COL Beer_Name VAL Bourbon Barrel Aged Panama Red Ale COL Brew_Factory_Name VAL Flossmoor Station Restaurant & Brewery COL Style VAL American Amber / Red Ale COL ABV VAL 7.00 % 	COL Beer_Name VAL [MASK] [MASK] [MASK] [MASK] COL Brew_Factory_Name VAL [MASK] [MASK] [MASK] [MASK] COL Style VAL [MASK] [MASK] [MASK] [MASK]  COL ABV VAL [MASK] % 	1"
+input = tokenizer(inputstring, return_tensors='pt', max_length=512, padding='max_length')
+
+outputs = model(**input)
